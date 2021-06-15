60eda4036eb1d06c16b5fcf2e91934c28b6a69fb
Oddie <romualdo.gui@gmail.com>
2021-06-15 17:22:28 -0300

    Add responsivity to the NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -6,9 +6,15 @@ import {
   IconButton,
   Container,
   Button,
+  useMediaQuery,
+  Menu,
+  MenuItem,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import ImgLogo from "../../assets/logotipoDOIS.png";
+import ImgLogo85 from "../../assets/logotipoDOIS85.png";
+import ImgLogo150 from "../../assets/logotipoDOIS150x112.png";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
@@ -23,6 +29,9 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: " black !important;",
     paddingLeft: "80px",
     paddingRight: "80px",
+    alignContent: "center",
+    flexWrap: "wrap",
+   
   },
   button: {
     display: "block;",
@@ -32,15 +41,57 @@ const useStyles = makeStyles((theme) => ({
     border: "none;",
     backgroundColor: "#00a8ff;",
     color: "#fff;",
-    fontSize: "18px;",
+    fontSize: "25px;",
     borderRadius: " 20px;",
     outline: "none;",
     cursor: "pointer;",
     transition: ".3s linear;",
     paddingLeft: "10px",
     marginLeft: "20px",
-    fontFamily: "'Roboto', sans-serif;",
     textTransform: "capitalize;",
+    fontFamily: "'Dosis', sans-serif;",
+    [theme.breakpoints.down("lg")]: {
+      margin: "21px 0 20px;",
+      marginLeft: "20px",      
+      fontSize: "18px;",
+      width: "200px;",
+      height: "60px;",
+    },
+    [theme.breakpoints.down("md")]: {
+      margin: "10px 0 10px;",
+      marginLeft: "20px",
+      fontSize: "17px;",
+      width: "150px;",
+      height: "60px;",
+    },
+    [theme.breakpoints.down("sm")]: {
+      margin: "10px 0 10px;",
+      marginLeft: "10px",
+      fontSize: "15px;",
+      width: "120px;",
+      height: "60px;",
+    },
+    [theme.breakpoints.down("426")]: {
+      margin: "10px 0 10px;",
+      marginLeft: "10px",
+      fontSize: "13px;",
+      width: "50px;",
+      height: "40px;",
+    },
+    [theme.breakpoints.down("376")]: {
+      margin: "6px 0 6px;",
+      marginLeft: "10px",
+      fontSize: "12px;",
+      width: "50px;",
+      height: "40px;",
+    },
+    [theme.breakpoints.down("321")]: {
+      margin: "4px 0 4px;",
+      marginLeft: "2px",  
+      fontSize: "10px;",    
+      width: "0px;",
+      height: "30px;",
+    },
   },
   btnHover1: {
     "&:hover": {
@@ -71,6 +122,7 @@ const useStyles = makeStyles((theme) => ({
     "&:hover": {
       boxShadow: " 0 0 10px #00a8ff",
       color: "#fff;",
+      fontSize: 175,
     },
   },
 
@@ -78,83 +130,306 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     /* backgroundColor: "green", */
     justifyContent: "flex-end",
+    
   },
   containerButtons: {
     display: "flex;",
     justifyContent: "space-around",
     flexDirection: "row;",
     /*  backgroundColor:"red", */
+    [theme.breakpoints.down("376")]: {
+      padding: "0"
+     },
   },
 }));
 
-function NavBar() {
+const ITEM_HEIGHT = 48;
+const Icons = 30;
+
+function NavBar(props) {
   const classes = useStyles();
+
+  //Breakpoint
+  const theme = useTheme();
+
+  const isMatch = useMediaQuery(theme.breakpoints.down("1023"));
+  const isMobile = useMediaQuery(theme.breakpoints.down("426"));
+  console.log(isMatch);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className={classes.grow}>
       <AppBar className={classes.appbar}>
         <Toolbar>
-          <Link href="">
-            <img src={ImgLogo} alt="logo" />
-          </Link>
-          <Container className={classes.containerButtons}>
-            <Button
-              className={`${classes.button} ${classes.btnHover1}`}
-              variant="contained"
-              color="primary"
-            >
-              Home
-            </Button>
-            <Button
-              className={`${classes.button} ${classes.btnHover2}`}
-              variant="contained"
-              color="primary"
-            >
-              Consult Patient
-            </Button>
-            <Button
-              className={`${classes.button} ${classes.btnHover3}`}
-              variant="contained"
-              color="primary"
-            >
-              Insert Patient
-            </Button>
-          </Container>
-          <Container className={classes.container}>
-            <IconButton
-              className={classes.IconButton}
-              href="https://www.facebook.com/MedcleanBR/"
-              target="_blank"
-              rel="noopener"
-              color="primary"
-              aria-label="facebook"
-            >
-              <FacebookIcon />
-            </IconButton>
+          {isMobile ? (
+            <>
+              <Link href="">
+                <img src={ImgLogo85} alt="logo" />
+              </Link>
+              <Container className={classes.containerButtons}>
+                    <Button
+                      className={`${classes.button} ${classes.btnHover1} }`}
+                      variant="contained"
+                      color="primary"
+                      onClick={() => props.addControl(1)}
+                    >
+                      Home
+                    </Button>
+                    <Button
+                      className={`${classes.button} ${classes.btnHover2} `}
+                      variant="contained"
+                      color="primary"
+                      onClick={() => props.addControl(2)}
+                    >
+                      Consult 
+                    </Button>
+                    <Button
+                      className={`${classes.button} ${classes.btnHover3} `}
+                      variant="contained"
+                      color="primary"
+                      onClick={() => props.addControl(3)}
+                    >
+                      Insert 
+                    </Button>
+                  </Container>
+                  <IconButton
+                    color="primary"
+                    aria-label="more"
+                    aria-controls="long-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                  >
+                    <MoreVertIcon style={{ fontSize: 22 }} />
+                  </IconButton>
+                  <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                      style: {
+                        maxHeight: ITEM_HEIGHT * 4.5,
+                        width: "20ch",
+                        background: "black",
+                      },
+                    }}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      <IconButton
+                        className={classes.IconButton}
+                        href="https://www.facebook.com/MedcleanBR/"
+                        target="_blank"
+                        rel="noopener"
+                        color="primary"
+                        aria-label="facebook"
+                      >
+                        <FacebookIcon />
+                      </IconButton>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <IconButton
+                        className={classes.IconButton}
+                        href="https://www.instagram.com/medclean_br/"
+                        target="_blank"
+                        rel="noopener"
+                        color="secondary"
+                        aria-label="instagram"
+                      >
+                        <InstagramIcon />
+                      </IconButton>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <IconButton
+                        className={classes.IconButton}
+                        href="https://br.linkedin.com/company/medclean"
+                        target="_blank"
+                        rel="noopener"
+                        color="inherit"
+                        aria-label="linkedin"
+                        style={{ color: lightBlue[500] }}
+                      >
+                        <LinkedInIcon />
+                      </IconButton>
+                    </MenuItem>
+                  </Menu>{" "}
+                </>
+          ) : (
+            <>
+              {isMatch ? (
+                <>
+                  <Link href="">
+                    <img src={ImgLogo150} alt="logo" />
+                  </Link>
+                  <Container className={classes.containerButtons}>
+                    <Button
+                      className={`${classes.button} ${classes.btnHover1} }`}
+                      variant="contained"
+                      color="primary"
+                      onClick={() => props.addControl(1)}
+                    >
+                      Home
+                    </Button>
+                    <Button
+                      className={`${classes.button} ${classes.btnHover2} `}
+                      variant="contained"
+                      color="primary"
+                      onClick={() => props.addControl(2)}
+                    >
+                      Consult Patient
+                    </Button>
+                    <Button
+                      className={`${classes.button} ${classes.btnHover3} `}
+                      variant="contained"
+                      color="primary"
+                      onClick={() => props.addControl(3)}
+                    >
+                      Insert Patient
+                    </Button>
+                  </Container>
+                  <IconButton
+                    color="primary"
+                    aria-label="more"
+                    aria-controls="long-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                  >
+                    <MoreVertIcon style={{ fontSize: Icons }} />
+                  </IconButton>
+                  <Menu
+                    id="long-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                      style: {
+                        maxHeight: ITEM_HEIGHT * 4.5,
+                        width: "20ch",
+                        background: "black",
+                      },
+                    }}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      <IconButton
+                        className={classes.IconButton}
+                        href="https://www.facebook.com/MedcleanBR/"
+                        target="_blank"
+                        rel="noopener"
+                        color="primary"
+                        aria-label="facebook"
+                      >
+                        <FacebookIcon />
+                      </IconButton>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <IconButton
+                        className={classes.IconButton}
+                        href="https://www.instagram.com/medclean_br/"
+                        target="_blank"
+                        rel="noopener"
+                        color="secondary"
+                        aria-label="instagram"
+                      >
+                        <InstagramIcon />
+                      </IconButton>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <IconButton
+                        className={classes.IconButton}
+                        href="https://br.linkedin.com/company/medclean"
+                        target="_blank"
+                        rel="noopener"
+                        color="inherit"
+                        aria-label="linkedin"
+                        style={{ color: lightBlue[500] }}
+                      >
+                        <LinkedInIcon />
+                      </IconButton>
+                    </MenuItem>
+                  </Menu>{" "}
+                </>
+              ) : (
+                <>
+                  {" "}
+                  <Link href="">
+                    <img src={ImgLogo} alt="logo" />
+                  </Link>
+                  <Container className={classes.containerButtons}>
+                    <Button
+                      className={`${classes.button} ${classes.btnHover1}`}
+                      variant="contained"
+                      color="primary"
+                      onClick={() => props.addControl(1)}
+                    >
+                      Home
+                    </Button>
+                    <Button
+                      className={`${classes.button} ${classes.btnHover2}`}
+                      variant="contained"
+                      color="primary"
+                      onClick={() => props.addControl(2)}
+                    >
+                      Consult Patient
+                    </Button>
+                    <Button
+                      className={`${classes.button} ${classes.btnHover3}`}
+                      variant="contained"
+                      color="primary"
+                      onClick={() => props.addControl(3)}
+                    >
+                      Insert Patient
+                    </Button>
+                  </Container>
+                  <Container className={classes.container}>
+                    <IconButton
+                      className={classes.IconButton}
+                      href="https://www.facebook.com/MedcleanBR/"
+                      target="_blank"
+                      rel="noopener"
+                      color="primary"
+                      aria-label="facebook"
+                    >
+                      <FacebookIcon />
+                    </IconButton>
 
-            <IconButton
-              className={classes.IconButton}
-              href="https://www.instagram.com/medclean_br/"
-              target="_blank"
-              rel="noopener"
-              color="secondary"
-              aria-label="instagram"
-            >
-              <InstagramIcon />
-            </IconButton>
+                    <IconButton
+                      className={classes.IconButton}
+                      href="https://www.instagram.com/medclean_br/"
+                      target="_blank"
+                      rel="noopener"
+                      color="secondary"
+                      aria-label="instagram"
+                    >
+                      <InstagramIcon />
+                    </IconButton>
 
-            <IconButton
-              className={classes.IconButton}
-              href="https://br.linkedin.com/company/medclean"
-              target="_blank"
-              rel="noopener"
-              color="inherit"
-              aria-label="linkedin"
-              style={{ color: lightBlue[500] }}
-            >
-              <LinkedInIcon />
-            </IconButton>
-          </Container>
+                    <IconButton
+                      className={classes.IconButton}
+                      href="https://br.linkedin.com/company/medclean"
+                      target="_blank"
+                      rel="noopener"
+                      color="inherit"
+                      aria-label="linkedin"
+                      style={{ color: lightBlue[500] }}
+                    >
+                      <LinkedInIcon />
+                    </IconButton>
+                  </Container>{" "}
+                </>
+              )}
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </div>
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,35 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import Cards from "../../components/Cards";
 import NavBar from "../../components/NavBar";
-import { Container } from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  cards: { 
+  cards: {
     margin: "40px 0;",
-    marginLeft: "20px",
   },
   container: {
     display: "flex",
     flexDirection: "row",
-    padding: "450px 100px",
-  
+    width: "100%",
+    height: "100%",
+    minHeight: "0px;",
+    minWidth: "0px;",
+    maxHeight: "none;",
+    maxWidth: "none;",
+    margin: "0px;",
+    padding: "0px",
+    paddingTop: "300px;",
+    [theme.breakpoints.down("376")]: {
+      paddingTop: "200px;",
+      width: "100%",
+    height: "100%",
+    },
   },
 }));
 
 function HomePage() {
   const classes = useStyles();
+  const [value, setValue] = useState(1);
+
+  function addControl(value) {
+    setValue(value);
+    console.log(value);
+  }
   return (
     <>
-      <NavBar />
-
-      <Container className={classes.container}>
-        
-        <Cards className={classes.cards} />
-        <Cards className={classes.cards} />
-        <Cards className={classes.cards} />
-        <Cards className={classes.cards} />
-      </Container>
+      <NavBar addControl={addControl} />
+      {value === 1 && (
+        <>
+          <Container className={classes.container}>
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+              spacing={3}
+            >
+              <Grid item xs={3} sm={12}>
+                <Cards className={classes.cards} />
+              </Grid>
+              <Grid item xs={3} sm={12}>
+                <Cards className={classes.cards} />
+              </Grid>
+              <Grid item xs={3} sm={12}>
+                <Cards className={classes.cards} />
+              </Grid>
+            </Grid>
+          </Container>
+        </>
+      )}
+      {value === 2 && (
+        <>
+          <Container className={classes.container}>teste AQui</Container>
+        </>
+      )}
+      {value === 3 && (
+        <>
+          <Container className={classes.container}>teste AQui2</Container>
+        </>
+      )}
     </>
   );
 }
